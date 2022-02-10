d6cc1fb86e089bf85eb5aa79830be1c8b39b976c
Erick Tovar <sephiroth_eri@hotmail.com>
2022-02-09 22:24:37 -0800

    1.2 code and tests

--- src/c1ArraysAndStrings/solutions.test.ts
+++ src/c1ArraysAndStrings/solutions.test.ts
@@ -1,4 +1,4 @@
-import { checaUnicidad } from "./solutions";
+import { checaUnicidad, checaPermutacion } from "./solutions";
 
 describe("1.1", () => {
   it("no dups", () => {
@@ -11,5 +11,28 @@ describe("1.1", () => {
     const prueba = "avbcdaefxr".split("");
     const result = checaUnicidad(prueba);
     expect(result).toBeTruthy();
+  });
+});
+
+describe("1.2", () => {
+  it("is permutation", () => {
+    const a = "pero".split("");
+    const b = "epro".split("");
+    const result = checaPermutacion(a, b);
+    expect(result).toBeTruthy();
+  });
+
+  it("not a permutation - different length", () => {
+    const a = "pero".split("");
+    const b = "repro".split("");
+    const result = checaPermutacion(a, b);
+    expect(result).toBeFalsy();
+  });
+
+  it("not a permutation - same length", () => {
+    const a = "pero".split("");
+    const b = "repr".split("");
+    const result = checaPermutacion(a, b);
+    expect(result).toBeFalsy();
   });
 });
--- src/c1ArraysAndStrings/solutions.ts
+++ src/c1ArraysAndStrings/solutions.ts
@@ -2,6 +2,7 @@
 
 // 1.1
 export function checaUnicidad(q: string[]): boolean {
+  // not using another data structure
   let hasDups = false;
   for (let indx = 0; indx < q.length; indx++) {
     for (let subIndx = indx + 1; subIndx < q.length; subIndx++) {
@@ -10,4 +11,29 @@ export function checaUnicidad(q: string[]): boolean {
     }
   }
   return hasDups;
+}
+
+//1.2
+export function checaPermutacion(a: string[], b: string[]): boolean {
+  if (a.length !== b.length) return false;
+
+  let isPermutation = true;
+  let letters = new Map<string, number>();
+
+  for (let indx = 0; indx < a.length; indx++) {
+    const prevCount = letters.get(a[indx]) || 0;
+    letters.set(a[indx], prevCount + 1);
+  }
+  for (let indx = 0; indx < b.length; indx++) {
+    if (!letters.has(b[indx])) return false;
+
+    const prevCount = letters.get(b[indx]) || 0;
+    letters.set(b[indx], prevCount - 1);
+  }
+
+  for (let value of letters.values()) {
+    if (value !== 0) return false;
+  }
+
+  return isPermutation;
 }
